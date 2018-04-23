10ccdd41399f11ac218de22626e8b49bbe9d6823
sbohmann <mail@sebastian-bohmann.com>
2018-04-23 13:43:12 +0200

    Processing md and image files

--- html_generator.py
+++ html_generator.py
@@ -8,20 +8,20 @@ class HtmlGenerator:
         self._image_files = []
 
     def run(self):
-        self.walk_source_directory()
-        self.transform_md_files()
-        self.copy_image_files()
+        self._walk_source_directory()
+        self._transform_md_files()
+        self._copy_image_files()
 
-    def walk_source_directory(self):
+    def _walk_source_directory(self):
         for directory, _, files in os.walk(self._configuration.source_directory):
             for file in files:
                 self._handle_file(os.path.join(directory, file))
 
     def _handle_file(self, file):
         if self._is_md_file(file):
-            self._md_files += file
+            self._md_files.append(file)
         elif self._is_image_file(file):
-            self._image_files += file
+            self._image_files.append(file)
 
     @staticmethod
     def _is_md_file(file):
@@ -33,8 +33,10 @@ class HtmlGenerator:
                 file.endswith('.jpg') or
                 file.endswith('png'))
 
-    def transform_md_files(self):
-        pass
+    def _transform_md_files(self):
+        for file in self._md_files:
+            print('md file:', file)
 
-    def copy_image_files(self):
-        pass
+    def _copy_image_files(self):
+        for file in self._image_files:
+            print('image file:', file)
