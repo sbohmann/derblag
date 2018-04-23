22fda7c477ff965b24fb4754e5abd278d8b93e68
sbohmann <mail@sebastian-bohmann.com>
2018-04-23 13:40:06 +0200

    vcs.xml

--- configuration_parser.py
+++ configuration_parser.py
@@ -6,11 +6,16 @@ class ConfigurationParser(object):
     source_directory_key = 'source-directory'
     destination_directory_key = 'destination-directory'
 
+    def __init__(self):
+        self.parser = self.create_parser()
+
     def run(self):
+        self.create_parser()
+        result = self.parser.parse_args()
+        return result
+
+    def create_parser(self):
         parser = argparse.ArgumentParser(allow_abbrev=False)
         parser.add_argument('-' + self.source_directory_key, type=str, required=True)
         parser.add_argument('-' + self.destination_directory_key, type=str, required=True)
-        result = parser.parse_args()
-        print(type(result))
-        print(result)
-        return result
+        return parser
--- html_generator.py
+++ html_generator.py
@@ -1,8 +1,40 @@
+import os
+
 
 class HtmlGenerator:
     def __init__(self, configuration):
-        self.configuration = configuration
+        self._configuration = configuration
+        self._md_files = []
+        self._image_files = []
 
     def run(self):
-        print(self.configuration.source_directory)
-        print(self.configuration.destination_directory)
+        self.walk_source_directory()
+        self.transform_md_files()
+        self.copy_image_files()
+
+    def walk_source_directory(self):
+        for directory, _, files in os.walk(self._configuration.source_directory):
+            for file in files:
+                self._handle_file(os.path.join(directory, file))
+
+    def _handle_file(self, file):
+        if self._is_md_file(file):
+            self._md_files += file
+        elif self._is_image_file(file):
+            self._image_files += file
+
+    @staticmethod
+    def _is_md_file(file):
+        return file.endswith('.md')
+
+    @staticmethod
+    def _is_image_file(file):
+        return (file.endswith('svg') or
+                file.endswith('.jpg') or
+                file.endswith('png'))
+
+    def transform_md_files(self):
+        pass
+
+    def copy_image_files(self):
+        pass
